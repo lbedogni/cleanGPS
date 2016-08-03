e7b67dd026763361ef59f82b9bda4af435e25fc8
lbedogni <lbedogni@cs.unibo.it>
2016-08-03 16:49:05 +0200

    New city

--- launch.sh
+++ launch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 #turin
-./toolchain_20160713.py $3 True 7.3 44.8 8 45.22 $1 $2 turin
+DANTIMESTART="20150304"
+DANTIMESTOP="20150305"
+if [ $2 == "turin" ]
+then
+	./toolchain_20160713.py $1 True 7.3 44.8 8 45.22 ${DANTIMESTART} ${DANTIMESTOP} turin
+elif [ $2 == "rome" ]
+then
+	./toolchain_20160713.py $1 True 11 40 13 43 ${DANTIMESTART} ${DANTIMESTOP} rome
+fi
