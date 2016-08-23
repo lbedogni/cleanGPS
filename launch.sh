22222555144458f71c1eff9734f3aea6607a31a6
lbedogni <lbedogni@cs.unibo.it>
2016-08-23 11:10:33 +0200

    Added a city

--- launch.sh
+++ launch.sh
@@ -5,8 +5,11 @@ DANTIMESTART="20150304"
 DANTIMESTOP="20150305"
 if [ $2 == "turin" ]
 then
-	./toolchain_20160713.py $1 True 7.3 44.8 8 45.22 ${DANTIMESTART} ${DANTIMESTOP} turin
+	./toolchain_20160713.py $1 True 44.8 7.3 45.22 8 ${DANTIMESTART} ${DANTIMESTOP} turin
 elif [ $2 == "rome" ]
 then
-	./toolchain_20160713.py $1 True 11 40 13 43 ${DANTIMESTART} ${DANTIMESTOP} rome
+	./toolchain_20160713.py $1 True 40 11 43 13 ${DANTIMESTART} ${DANTIMESTOP} rome
+elif [ $2 == "newyork" ]
+then
+	./toolchain_20160713.py $1 True 39.5 -75 42 -72 20131204 20131205 nyc
 fi
