2087ed0b6700f3893609ab9bbe494b606fea248b
lbedogni <lbedogni@cs.unibo.it>
2016-08-27 11:43:31 +0200

    Added all cities

--- launch.sh
+++ launch.sh
@@ -3,6 +3,7 @@
 #turin
 DANTIMESTART="20150304"
 DANTIMESTOP="20150305"
+touch ${2}.cache.nearest.csv
 if [ $2 == "turin" ]
 then
 	./toolchain_20160713.py $1 True 44.8 7.3 45.22 8 ${DANTIMESTART} ${DANTIMESTOP} turin
@@ -12,4 +13,22 @@ then
 elif [ $2 == "newyork" ]
 then
 	./toolchain_20160713.py $1 True 39.5 -75 42 -72 20131204 20131205 nyc
+elif [ $2 == "milan" ]
+then
+	./toolchain_20160713.py $1 True 45 8.5 46 10 ${DANTIMESTART} ${DANTIMESTOP} milan
+elif [ $2 == "bari" ]
+then
+	./toolchain_20160713.py $1 True 40.8 16.5 41.2 17.1 ${DANTIMESTART} ${DANTIMESTOP} bari
+elif [ $2 == "palermo" ]
+then
+	./toolchain_20160713.py $1 True 38 13 38.5 13.5 ${DANTIMESTART} ${DANTIMESTOP} palermo
+elif [ $2 == "sanfrancisco" ]
+then
+	./toolchain_20160713.py $1 True 37.5 -122.6 38 -122.2 20080428 20080429 sanfrancisco
+elif [ $2 == "beijing" ]
+then
+	./toolchain_20160713.py $1 True 39.3 115.2 41.1 117.6 20080205 20080206 beijing
+elif [ $2 == "shanghai" ]
+then
+	./toolchain_20160713.py $1 True 30.5 120.5 32 122.2 20080205 20080206 beijing
 fi
